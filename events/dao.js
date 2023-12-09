63b8c75a3d7aca0131e44b394adc183fd15548f2
Tanya Batra <tanyabatra@Tanyas-MacBook-Pro.local>
2023-12-09 17:40:24 -0500

    dashboard apis modified

--- events/dao.js
+++ events/dao.js
@@ -2,5 +2,8 @@ import model from "./model.js"
 
 export const createEvent = (event) => model.create(event);
 export const findAllEvents = () => model.find();
+// export const findEventById = (eventId) => model.findById(eventId);
+export const findEventById = (eventId) => model.findById(eventId);
+
 
 
--- events/routes.js
+++ events/routes.js
@@ -3,7 +3,7 @@ import * as dao from "./dao.js";
 //import { ObjectID } from 'mongodb'; // Import ObjectID for MongoDB
 
 function EventRoutes(app) {
- // console.log("In event routes");
+  // console.log("In event routes");
   const createEvent = async (req, res) => {
     const event = await dao.createEvent(req.body);
     res.json(event);
@@ -12,21 +12,30 @@ function EventRoutes(app) {
   const findAllEvents = async (req, res) => {
     try {
       const events = await dao.findAllEvents();
-      console.log("The events are", events);
+      // console.log("The events are", events);
       res.json(events);
     } catch (error) {
       console.error("Error fetching events:", error);
       res.status(500).json({ error: "Internal Server Error" });
     }
   };
-  
+
+  const findEventById = async (req, res) => {
+    try {
+      const event = await dao.findEventById(req.params.eventId);
+      console.log("The events are", event);
+      res.json(event);
+    } catch (error) {
+      console.error("Error fetching event details:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
 
   // Add more event-related routes as needed
-
+  app.get("/api/events/:eventId", findEventById);
   app.post("/api/events", createEvent);
   app.get("/api/events", findAllEvents);
   // Add more event-related routes
-
 }
 
 export default EventRoutes;
