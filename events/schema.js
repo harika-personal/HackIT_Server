b6f27520571284aebd50e06229fd3284c3ef9899
Tanya Batra <tanyabatra@Tanyas-MacBook-Pro.local>
2023-12-10 12:45:45 -0500

    modified

--- events/schema.js
+++ events/schema.js
@@ -1,66 +1,55 @@
-
 import mongoose from "mongoose";
 
-const eventSchema = new mongoose.Schema({
-  organizerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  organizerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  numberOfSpots: {
-    type: Number,
-    required: true,
-  },
-  eventName: {
-    type: String,
-    required: true,
-  },
-  summary: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  duration: {
-    type: String,
-    required:true,
-  },
-  timeStart: {
-    type: String,
-    required: true,
-  },
-  timeEnd: {
-    type: String,
-    required: true, 
-  },
-  venue: {
-    type: String,
-    required: true, 
-  },
-  photo: {
-    type: String,
-    required: true,
-  },
-  ratings: {
-    overallRating: {
-      type: Number,
+const eventSchema = new mongoose.Schema(
+  {
+    eventName: {
+      type: String,
       required: true,
     },
-    numberOfRates: {
-      type: Number,
+    summary: {
+      type: String,
       required: true,
     },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    timeStart: {
+      type: String,
+      required: false,
+    },
+    timeEnd: {
+      type: String,
+      required: false,
+    },
+    venue: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    ratings: {
+      overallRating: {
+        type: Number,
+        required: false,
+      },
+      numberOfRates: {
+        type: Number,
+        required: false,
+      },
+    },
   },
-},
-{collection: "Events"});
+  { collection: "Events" }
+);
 
 export default eventSchema;
