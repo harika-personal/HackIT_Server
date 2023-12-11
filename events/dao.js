c904995df23d0624931002c744d72444b4ef0b5b
Tanya Batra <tanyabatra@Tanyas-MacBook-Pro.local>
2023-12-10 20:07:49 -0500

    modified

--- events/dao.js
+++ events/dao.js
@@ -2,8 +2,4 @@ import model from "./model.js"
 
 export const createEvent = (event) => model.create(event);
 export const findAllEvents = () => model.find();
-// export const findEventById = (eventId) => model.findById(eventId);
 export const findEventById = (eventId) => model.findById(eventId);
-
-
-
--- events/routes.js
+++ events/routes.js
@@ -1,13 +1,10 @@
 // Import the necessary modules
 import * as dao from "./dao.js";
-//import { ObjectID } from 'mongodb'; // Import ObjectID for MongoDB
 
 function EventRoutes(app) {
-  // console.log("In event routes");
   const findAllEvents = async (req, res) => {
     try {
       const events = await dao.findAllEvents();
-      // console.log("The events are", events);
       res.json(events);
     } catch (error) {
       console.error("Error fetching events:", error);
@@ -28,7 +25,6 @@ function EventRoutes(app) {
 
   const createEvent = async (req, res) => {
     try {
-      // Create the event and send it as a JSON response
       const event = await dao.createEvent(req.body);
       res.json(event);
     } catch (error) {
@@ -38,12 +34,9 @@ function EventRoutes(app) {
   };
 
   app.post("/api/events", createEvent);
-
-  // Add more event-related routes as needed
   app.get("/api/events/:eventId", findEventById);
   app.post("/api/events", createEvent);
   app.get("/api/events", findAllEvents);
-  // Add more event-related routes
 }
 
 export default EventRoutes;
