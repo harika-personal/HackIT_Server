be5535200fb9dc3ce62407ef8b2d23f6a807d9d0
Snigdha Bose <62890614+snigdhab7@users.noreply.github.com>
2023-12-13 14:46:05 -0500

    signup

--- users/routes.js
+++ users/routes.js
@@ -18,9 +18,9 @@ function UserRoutes(app) {
   };
 
   const findUserById = async (req, res) => {
-    console.log("SSS",req)
-    console.log("SSSB",req.userId)
-    console.log("SSSBB",req.params.userId)//getting
+    //console.log("SSS",req)
+    //console.log("SSSB",req.userId)
+    //console.log("SSSBB",req.params.userId)//getting
     const user = await dao.findUserById(req.userId);
     res.json(user);
   };
@@ -45,20 +45,54 @@ function UserRoutes(app) {
         { message: "Username already taken" });
     }
     const currentUser = await dao.createUser(req.body);
-    console.log("fv",currentUser)
+    //console.log("fv",currentUser)
    //req.session['currentUser'] = currentUser;
 
     res.json(currentUser);
   };
 
-  const signin = async (req, res) => {
+  const signin1 = async (req, res) => {
     const { username, password } = req.body;
    // console.log("SIGNINuss", username)
     const currentUser = await dao.findUserByCredentials(username, password);
-    console.log("snighdhaBose",currentUser)
+    console.log("snighdhaBose1",currentUser.role)
    //req.session['currentUser'] = currentUser;
 
     res.json(currentUser);
+  };
+
+  const signinUser = async (req, res) => {
+    const { username, password } = req.body;
+   // console.log("SIGNINuss", username)
+    const currentUser = await dao.findUserByCredentials(username, password);
+   
+    if(currentUser && currentUser.role && currentUser.role=='user'){
+      console.log("snighdhaBose2",currentUser)
+      console.log("snighdhaBose3",currentUser.role)
+      res.json(currentUser);
+    }else{
+      res.json(null);
+    }
+
+    
+   //req.session['currentUser'] = currentUser;
+
+   // res.json(currentUser);
+  };
+
+  const signinOrganizer = async (req, res) => {
+    const { username, password } = req.body;
+   // console.log("SIGNINuss", username)
+    const currentUser = await dao.findUserByCredentials(username, password);
+    console.log("snighdhaBose4",currentUser)
+    
+    if(currentUser && currentUser.role && currentUser.role=='organizer'){
+      console.log("snighdhaBose5",currentUser.role)
+      res.json(currentUser);
+    }else{
+      res.json(null);
+    }
+
   };
 
    const signout = (req, res) => {
@@ -78,7 +112,8 @@ function UserRoutes(app) {
   app.put("/api/users/:userId", updateUser);
   app.delete("/api/users/:userId", deleteUser);
   app.post("/api/users/signup", signup);
-  app.post("/api/users/signin", signin);
+  app.post("/api/users/signin/user", signinUser);
+  app.post("/api/users/signin/organizer", signinOrganizer);
   app.post("/api/users/signout", signout);
   app.post("/api/users/account", account);
 }
