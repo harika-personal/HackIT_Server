e827cd3ca4e5729c1d551c38340e735627ee697c
Snigdha Bose <62890614+snigdhab7@users.noreply.github.com>
2023-12-13 21:37:50 -0500

    uu

--- events/routes.js
+++ events/routes.js
@@ -12,6 +12,33 @@ function EventRoutes(app) {
     }
   };
 
+  const findAllOrganizerEvents = async (req, res) => {
+    try {
+      const allEvents = await dao.findAllEvents();
+      console.log("000003", req.params.organizerId);
+      console.log("777", allEvents);
+  
+      // Filter events based on organizerid
+      const filteredEvents = allEvents.filter(event => {
+        console.log("HHHHIIIIIII",typeof event.organizerId, typeof req.params.organizerId);
+        //const extractedId = objectId.toString();
+
+//console.log(extractedId);
+console.log("AAAAA",event._id);
+        console.log("BBBBB",event.organizerId);
+        return event.organizerId === req.params.organizerId;
+      });
+  
+      console.log("444", filteredEvents);
+      res.json(filteredEvents);
+    } catch (error) {
+      console.error("Error fetching events:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
+  
+  
+
   const findEventById = async (req, res) => {
     try {
       const event = await dao.findEventById(req.params.eventId);
@@ -47,6 +74,7 @@ function EventRoutes(app) {
 
   app.post("/api/events", createEvent);
   app.get("/api/events/:eventId", findEventById);
+  app.get("/api/events/organizer/:organizerId", findAllOrganizerEvents);
   app.post("/api/events", createEvent);
   app.get("/api/events", findAllEvents);
 }
